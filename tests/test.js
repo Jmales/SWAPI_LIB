d4a6e7a89dbd00dea904643b3397c55f1a3fa8b5
Joao Males <jpmp9@hotmail.com>
2017-05-11 19:22:56 +0100

    Corrected minor details.

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,4 @@
-/**Tests should go here.
+/**Tests should go here. Either using Jasmine, QUnit, Mocha, etc.
  * I didn't think it was necessary to do them. 
  * I performed some tests in Firefox Developer Tools, but
  * if it is necessary please tell me and I will complement my answer with some tests.
