8455abeadf9dab1840ac9ee558ce659a8d3d6234
Joao Males <jpmp9@hotmail.com>
2017-05-11 02:28:28 +0100

    requestMultiple() function is now integrated in request()

---
+++ tests/test.js
@@ -0,0 +1,5 @@
+/**Tests should go here.
+ * I didn't think it was necessary to do them. 
+ * I performed some tests in Firefox Developer Tools, but
+ * if it is necessary please tell me and I will complement my answer with some tests.
+ */
